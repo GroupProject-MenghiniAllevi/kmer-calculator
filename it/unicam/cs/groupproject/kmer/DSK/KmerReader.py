45bebaace10409eb5b112baf6714cc9e2247de4a
davide menghini <davide.menghini@studenti.unicam.it>
2022-08-22 11:47:41 +0200

    aggiunti commenti all'interfaccia KmerReader

--- it/unicam/cs/groupproject/kmer/DSK/KmerReader.py
+++ it/unicam/cs/groupproject/kmer/DSK/KmerReader.py
@@ -2,3 +2,26 @@
 
 class KmerReader:
     pass
+
+
+    def read_next_kmer(self):
+        """
+        Questo metodo permette di leggere il prossimo kmer nel file
+        :return: una stringa contenente il kmer
+        """
+
+
+    def set_kmer_lenght(self,k):
+        """
+        Questo metodo imposta la lunghezza del kmer da leggere
+        :param k: la lunchezza del kmer.
+        :return:
+        """
+
+
+    def set_path(self, path):
+        """
+        
+        :param path:
+        :return:
+        """
